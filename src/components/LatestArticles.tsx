dac19e2d8e5f2360bf6949f101941b792f594559
Hrvoje <hrvoje.jakopovic@sistemi.hr>
2023-01-09 17:29:14 +0100

    infinite scroll

--- src/components/LatestArticles.tsx
+++ src/components/LatestArticles.tsx
@@ -1,85 +1,85 @@
-import ArticleLatest from "./ArticleLatest"
-import '../styles/LatestArticles.scss';
-import axios from 'axios'
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { useState, useRef, useEffect } from "react";
-import { useInView } from 'react-intersection-observer'
+import ArticleLatest from "./ArticleLatest";
+import "../styles/LatestArticles.scss";
+import axios from "axios";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useRef, useEffect, useMemo } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const LatestArticles = ()=>{
-    const [page, setPage] = useState(1)
-    const scrollWin = useRef<HTMLDivElement>(null);
-    const { ref, inView } = useInView()
-
-
-    const {
-        status,
-        data,
-        error,
-        isFetching,
-        isFetchingNextPage,
-        isFetchingPreviousPage,
-        fetchNextPage,
-        fetchPreviousPage,
-        hasNextPage,
-        hasPreviousPage,
-      } = useInfiniteQuery(
-        ['projects'],
-        async ({ pageParam = 1 }) => {
-          const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&page=${pageParam}apiKey=1b29c9ede1f741e0b67f5bcd3c834b1a`)
-          return res.data
-        },
-        {
-          getPreviousPageParam: (firstPage) => firstPage.previousId ?? undefined,
-          getNextPageParam: (lastPage) => lastPage.nextId ?? undefined,
-        },
+const LatestArticles = () => {
+  const scrollWindow = useRef<HTMLDivElement>(null);
+  const fetchProjects = ({ pageParam = 1 }) => {
+    return axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&page=${pageParam}&pageSize=10&apiKey=1b29c9ede1f741e0b67f5bcd3c834b1a`
       )
+      .then((res) => res.data);
+  };
 
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status
+  } = useInfiniteQuery({
+    queryKey: ["latest"],
+    queryFn: fetchProjects,
+    getNextPageParam: (lastPage, pages) => {
+      const maxPages = lastPage.totalResults / 10 + 1;
+      const nextPage = pages.length + 1;
+      return nextPage <= maxPages ? nextPage : undefined;
+    }
+  });
 
-      useEffect(() => {
-        if (inView) {
-          fetchNextPage()
-        }
-      }, [inView])
+  const characters = useMemo(
+    () =>
+      data?.pages
+        .flatMap((page: any) => page.articles)
+        .map((ar: any) => {
+          const hour = new Date(ar.publishedAt).getHours().toString();
+          const min = new Date(ar.publishedAt).getMinutes().toString();
+          return {
+            uri: ar.url,
+            title: ar.title,
+            hours: hour.length === 1 ? "0" + hour : hour,
+            minutes: min.length === 1 ? "0" + min : min
+          };
+        }),
+    [data]
+  );
+  const more = characters?.length === data?.pages[0].totalResults;
 
-//     const {data: latestData, isLoading: latestLoading, isError: latestError} = useQuery(['latest'],
-//     ()=>{
-//      return axios.get(`https://newsapi.org/v2/top-headlines?country=us&page=${page}apiKey=1b29c9ede1f741e0b67f5bcd3c834b1a`).then(res=>res.data)
-//     }, { staleTime: 1000}
-//    )
+  if (status === "loading") return <p>Loading...</p>;
 
-    if ( status==='loading'){
-        return <h1>Loading...</h1>
-      }
-    
-    const latestArticleData = [...data.articles.map((ar:any)=>{
-        const hour = new Date(ar.publishedAt).getHours().toString()
-        const min =new Date(ar.publishedAt).getMinutes().toString()
-        return {
-          uri: ar.url,
-          title: ar.title,
-          hours: hour.length===1 ?'0'+ hour:hour , 
-          minutes: min.length===1 ?'0'+ min:min 
-        }
-      })]
+  if (status === "error") return <p>Error...</p>;
 
-    return(
-        <div className="l-latest-articles-wrapper">
-            <h3 className="c-latest-articles-wrap-title">Latest news</h3>
-            <section className="c-latest-articles-wrap">
-                <div className="c-latest-articles" ref={scrollWin}>
-                    {
-                        latestArticleData.map((article:any)=>(
-                        <ArticleLatest article={article} />
-                        ))
-                    }
-                </div>
-            </section>
-            <div className='c-latest-articles-btn-wrap'>
-                <button className="c-latest-articles-btn">See all news</button>
-            </div>
+  return (
+    <div className="l-latest-articles-wrapper">
+      <h3 className="c-latest-articles-wrap-title">Latest news</h3>
+      <section className="c-latest-articles-wrap" id="scrollableDiv">
+        <div className="c-latest-articles" ref={scrollWindow}>
+          <InfiniteScroll
+            dataLength={characters ? characters.length : 0}
+            next={() => fetchNextPage()}
+            hasMore={!more}
+            loader={<h4>Loading...</h4>}
+            scrollableTarget="scrollableDiv"
+          >
+            {characters?.map((article: any) => (
+              <ArticleLatest article={article} />
+            ))}
+          </InfiniteScroll>
+
+          <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
         </div>
-           
-    )
-}
+      </section>
+      <div className="c-latest-articles-btn-wrap">
+        <button className="c-latest-articles-btn">See all news</button>
+      </div>
+    </div>
+  );
+};
 
-export default LatestArticles
+export default LatestArticles;
